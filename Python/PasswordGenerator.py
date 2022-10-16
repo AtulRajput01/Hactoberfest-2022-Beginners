956c8a44836b5aee3161055dbd12387e28154d3f
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2022-10-16 22:07:48 +0530

    Update PasswordGenerator.py

--- Python/PasswordGenerator.py
+++ Python/PasswordGenerator.py
@@ -1,63 +1,59 @@
 import random
-Function for generating a password
+import array
 
-def generatePassword(pwlength):
+# maximum length of password needed
+# this can be changed to suit your password length
+MAX_LEN = 12
 
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
+# declare arrays of the character that we need in out password
+# Represented as chars to enable easy string concatenation
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+					'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+					'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+					'z']
 
-    passwords = [] 
+UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+					'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q',
+					'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+					'Z']
 
-    for i in pwlength:
-        
-        password = "" 
-        for j in range(i):
-            next_letter_index = random.randrange(len(alphabet))
-            password = password + alphabet[next_letter_index]
-        
-        password = replaceWithNumber(password)
-        password = replaceWithUppercaseLetter(password)
-        
-        passwords.append(password) 
-    
-    return passwords
-Function for replaceWithNumber
+SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+		'*', '(', ')', '<']
 
-def replaceWithNumber(pword):
-    for i in range(random.randrange(1,3)):
-        replace_index = random.randrange(len(pword)//2)
-        pword = pword[0:replace_index] + str(random.randrange(10)) + pword[replace_index+1:]
-        return pword
-Function for replaceWithUpperCaseLetter
+# combines all the character arrays above to form one array
+COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
 
+# randomly select at least one character from each character set above
+rand_digit = random.choice(DIGITS)
+rand_upper = random.choice(UPCASE_CHARACTERS)
+rand_lower = random.choice(LOCASE_CHARACTERS)
+rand_symbol = random.choice(SYMBOLS)
 
-def replaceWithUppercaseLetter(pword):
-    for i in range(random.randrange(1,3)):
-        replace_index = random.randrange(len(pword)//2,len(pword))
-        pword = pword[0:replace_index] + pword[replace_index].upper() + pword[replace_index+1:]
-        return pword
-Main function
+# combine the character randomly selected above
+# at this stage, the password contains only 4 characters but
+# we want a 12-character password
+temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
 
 
-def main():
-    
-    numPasswords = int(input("How many passwords do you want to generate? "))
-    
-    print("Generating " +str(numPasswords)+" passwords")
-    
-    passwordLengths = []
+# now that we are sure we have at least one character from each
+# set of characters, we fill the rest of
+# the password length by selecting randomly from the combined
+# list of character above.
+for x in range(MAX_LEN - 4):
+	temp_pass = temp_pass + random.choice(COMBINED_LIST)
 
-    print("Minimum length of password should be 3")
+	# convert temporary password into array and shuffle to
+	# prevent it from having a consistent pattern
+	# where the beginning of the password is predictable
+	temp_pass_list = array.array('u', temp_pass)
+	random.shuffle(temp_pass_list)
 
-    for i in range(numPasswords):
-        length = int(input("Enter the length of Password #" + str(i+1) + " "))
-        if length<3:
-            length = 3
-        passwordLengths.append(length)
-    
-    
-    Password = generatePassword(passwordLengths)
-
-    for i in range(numPasswords):
-        print ("Password #"+str(i+1)+" = " + Password[i])
-
-main()
+# traverse the temporary password array and append the chars
+# to form the password
+password = ""
+for x in temp_pass_list:
+		password = password + x
+		
+# print out password
+print(password)
